e83f4bb4e60cb5cc05a2b4f917785d8d8745e38d
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-08-28 14:55:51 +0000

    rootパス追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
 
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -16,10 +12,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root to: 'public/homes#top'
+
   scope module: :public do
     resources :customers, except: [:new]
     # get 'cusromers/index'
     # get 'cusromers/show'
     # get 'cusromers/edit'
+    resources :homes, only: [:top, :about]
+    # get 'homes/top'
+    # get 'homes/about'
   end
 end
